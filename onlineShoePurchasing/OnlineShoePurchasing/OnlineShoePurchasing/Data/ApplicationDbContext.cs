f14f8bcbfe004a8225748f1f7b42f649bb457aa2
100827 <100827@MACTECH.LOC>
2025-05-30 09:06:30 +0530

    fghj

---
+++ onlineShoePurchasing/OnlineShoePurchasing/OnlineShoePurchasing/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShoePurchasing.Models;
+
+namespace OnlineShoePurchasing.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Logdetails> logindetails { get; set; }
+        public DbSet<shoeorderDtl> Shoesdetails { get; set; }
+        public DbSet<shoeorderDtl> shoeorderDetails { get; set; }
+
+    }
+}
